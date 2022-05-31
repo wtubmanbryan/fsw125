3f516d9188111632f655cac16cd9230fed9a0e2b
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-05-31 01:54:37 -0400

    Added Wk1 - Node Module Basics and Patterns

---
+++ wk1/app.js
@@ -0,0 +1,8 @@
+var compute = require('./math.js');
+
+compute.add(200, 400);
+
+compute.mul(200, 400);
+compute.sub(600, 400);
+compute.div(200, 3);
+
---
+++ wk1/math.js
@@ -0,0 +1,22 @@
+// Function for Addition
+module.exports.add = function (num1, num2) { 
+    console.log(num1 + num2);
+};
+
+
+//Function for Multiplication
+module.exports.mul = function (num1, num2) { 
+    console.log(num1 * num2);
+};
+
+
+// Function for Substraction
+
+module.exports.sub = function (num1, num2) { 
+    console.log(num1 - num2);
+};
+
+//Fucntion for Divison
+module.exports.div = function (num1, num2) { 
+    console.log(num1 / num2);
+};
